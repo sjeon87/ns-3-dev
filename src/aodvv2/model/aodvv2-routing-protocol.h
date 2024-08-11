bde94ceed1d25922a6c2db7720865d8d04a1331c
Francesco <todinofrancesco97@gmail.com>
2024-08-11 17:37:02 -0500

    fix clang format

--- src/aodvv2/model/aodvv2-routing-protocol.h
+++ src/aodvv2/model/aodvv2-routing-protocol.h
@@ -442,7 +442,9 @@ class RoutingProtocol : public Ipv4RoutingProtocol
      * \param rreqHeader route request header
      * \param toOrigin routing table entry to originator
      */
-    void SendReply(const RreqHeader& rreqHeader, const RoutingTableEntry& toOrigin, uint8_t hopCount);
+    void SendReply(const RreqHeader& rreqHeader,
+                   const RoutingTableEntry& toOrigin,
+                   uint8_t hopCount);
     /** Send RREP by intermediate node
      * \param toDst routing table entry to destination
      * \param toOrigin routing table entry to originator
