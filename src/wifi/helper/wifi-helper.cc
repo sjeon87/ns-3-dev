ef5a04ae1dba2f7c7153720bd2f91785ee9bdf3e
Sébastien Deronne <sebastien.deronne@gmail.com>
2024-12-28 16:58:11 +0100

    wifi: Add minimal support for UHR PPDUs

--- src/wifi/helper/wifi-helper.cc
+++ src/wifi/helper/wifi-helper.cc
@@ -1287,6 +1287,7 @@ WifiHelper::EnableLogComponents(LogLevel logLevel)
     LogComponentEnable("ThresholdPreambleDetectionModel", logLevel);
     LogComponentEnable("Txop", logLevel);
     LogComponentEnable("UhrConfiguration", logLevel);
+    LogComponentEnable("UhrPpdu", logLevel);
     LogComponentEnable("VhtConfiguration", logLevel);
     LogComponentEnable("VhtFrameExchangeManager", logLevel);
     LogComponentEnable("VhtPhy", logLevel);
