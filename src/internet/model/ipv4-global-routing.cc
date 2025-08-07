sjeon87/ns-3-dev

commit b7619e2ab00bb3912c887f7ba8b8ee9f9942108c

internet: Do NOT Add identical route entries in Routing table for GlobalRouting

src/internet/model/ipv4-global-routing.cc
@@ -71,6 +71,14 @@ Ipv4GlobalRouting::AddHostRouteTo(Ipv4Address dest, Ipv4Address nextHop, uint32_
     NS_LOG_FUNCTION(this << dest << nextHop << interface);
     auto route = new Ipv4RoutingTableEntry();
     *route = Ipv4RoutingTableEntry::CreateHostRouteTo(dest, nextHop, interface);
+    for (auto routePointer : m_hostRoutes)
+    {
+        if (*routePointer == *route)
+        {
+            NS_LOG_LOGIC("Route already exists");
+            return;
+        }
+    }
     m_hostRoutes.push_back(route);
 }
 
@@ -80,6 +88,14 @@ Ipv4GlobalRouting::AddHostRouteTo(Ipv4Address dest, uint32_t interface)
     NS_LOG_FUNCTION(this << dest << interface);
     auto route = new Ipv4RoutingTableEntry();
     *route = Ipv4RoutingTableEntry::CreateHostRouteTo(dest, interface);
+    for (auto routePointer : m_hostRoutes)
+    {
+        if (*routePointer == *route)
+        {
+            NS_LOG_LOGIC("Route already exists");
+            return;
+        }
+    }
     m_hostRoutes.push_back(route);
 }
 
@@ -92,6 +108,14 @@ Ipv4GlobalRouting::AddNetworkRouteTo(Ipv4Address network,
     NS_LOG_FUNCTION(this << network << networkMask << nextHop << interface);
     auto route = new Ipv4RoutingTableEntry();
     *route = Ipv4RoutingTableEntry::CreateNetworkRouteTo(network, networkMask, nextHop, interface);
+    for (auto routePointer : m_networkRoutes)
+    {
+        if (*routePointer == *route)
+        {
+            NS_LOG_LOGIC("Route already exists");
+            return;
+        }
+    }
     m_networkRoutes.push_back(route);
 }
 
@@ -101,6 +125,14 @@ Ipv4GlobalRouting::AddNetworkRouteTo(Ipv4Address network, Ipv4Mask networkMask, 
     NS_LOG_FUNCTION(this << network << networkMask << interface);
     auto route = new Ipv4RoutingTableEntry();
     *route = Ipv4RoutingTableEntry::CreateNetworkRouteTo(network, networkMask, interface);
+    for (auto routePointer : m_networkRoutes)
+    {
+        if (*routePointer == *route)
+        {
+            NS_LOG_LOGIC("Route already exists");
+            return;
+        }
+    }
     m_networkRoutes.push_back(route);
 }
 
@@ -113,6 +145,14 @@ Ipv4GlobalRouting::AddASExternalRouteTo(Ipv4Address network,
     NS_LOG_FUNCTION(this << network << networkMask << nextHop << interface);
     auto route = new Ipv4RoutingTableEntry();
     *route = Ipv4RoutingTableEntry::CreateNetworkRouteTo(network, networkMask, nextHop, interface);
+    for (auto routePointer : m_ASexternalRoutes)
+    {
+        if (*routePointer == *route)
+        {
+            NS_LOG_LOGIC("Route already exists");
+            return;
+        }
+    }
     m_ASexternalRoutes.push_back(route);
 }
 

commit 228238bc06d39fa065fa460f598fcc7c7f0fecdc

internet: fix valgrind error in globalrouting caused by b7619e2

src/internet/model/ipv4-global-routing.cc
@@ -76,6 +76,7 @@ Ipv4GlobalRouting::AddHostRouteTo(Ipv4Address dest, Ipv4Address nextHop, uint32_
         if (*routePointer == *route)
         {
             NS_LOG_LOGIC("Route already exists");
+            delete route;
             return;
         }
     }
@@ -93,6 +94,7 @@ Ipv4GlobalRouting::AddHostRouteTo(Ipv4Address dest, uint32_t interface)
         if (*routePointer == *route)
         {
             NS_LOG_LOGIC("Route already exists");
+            delete route;
             return;
         }
     }
@@ -113,6 +115,7 @@ Ipv4GlobalRouting::AddNetworkRouteTo(Ipv4Address network,
         if (*routePointer == *route)
         {
             NS_LOG_LOGIC("Route already exists");
+            delete route;
             return;
         }
     }
@@ -130,6 +133,7 @@ Ipv4GlobalRouting::AddNetworkRouteTo(Ipv4Address network, Ipv4Mask networkMask, 
         if (*routePointer == *route)
         {
             NS_LOG_LOGIC("Route already exists");
+            delete route;
             return;
         }
     }
@@ -150,6 +154,7 @@ Ipv4GlobalRouting::AddASExternalRouteTo(Ipv4Address network,
         if (*routePointer == *route)
         {
             NS_LOG_LOGIC("Route already exists");
+            delete route;
             return;
         }
     }