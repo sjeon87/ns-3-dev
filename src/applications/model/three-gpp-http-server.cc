nsnam/ns-3-dev-git

applications: Move protocol TypeId to sink application base class

src/applications/model/three-gpp-http-server.cc
@@ -40,6 +40,7 @@ ThreeGppHttpServer::ThreeGppHttpServer()
 {
     NS_LOG_FUNCTION(this);
     NS_LOG_INFO(this << " MTU size for this server application is " << m_mtuSize << " bytes.");
+    m_tid = TypeId::LookupByName("ns3::TcpSocketFactory");
 }
 
 // static
@@ -223,7 +224,7 @@ ThreeGppHttpServer::StartApplication()
     if (!m_socket)
     {
         // Creating a TCP socket to connect to the server.
-        m_socket = Socket::CreateSocket(GetNode(), TcpSocketFactory::GetTypeId());
+        m_socket = Socket::CreateSocket(GetNode(), m_tid);
         m_socket->SetAttribute("SegmentSize", UintegerValue(m_mtuSize));
 
         NS_ABORT_MSG_IF(m_local.IsInvalid(), "Local address not properly set");

src/applications/model/udp-echo-server.cc
@@ -52,6 +52,7 @@ UdpEchoServer::UdpEchoServer()
     : SinkApplication(DEFAULT_PORT)
 {
     NS_LOG_FUNCTION(this);
+    m_tid = TypeId::LookupByName("ns3::UdpSocketFactory");
 }
 
 UdpEchoServer::~UdpEchoServer()
@@ -66,8 +67,7 @@ UdpEchoServer::StartApplication()
 
     if (!m_socket)
     {
-        auto tid = TypeId::LookupByName("ns3::UdpSocketFactory");
-        m_socket = Socket::CreateSocket(GetNode(), tid);
+        m_socket = Socket::CreateSocket(GetNode(), m_tid);
         auto local = m_local;
         if (local.IsInvalid())
         {
@@ -98,8 +98,7 @@ UdpEchoServer::StartApplication()
     {
         // local address is not specified, so create another socket to also listen to all IPv6
         // addresses
-        auto tid = TypeId::LookupByName("ns3::UdpSocketFactory");
-        m_socket6 = Socket::CreateSocket(GetNode(), tid);
+        m_socket6 = Socket::CreateSocket(GetNode(), m_tid);
         auto local = Inet6SocketAddress(Ipv6Address::GetAny(), m_port);
         if (m_socket6->Bind(local) == -1)
         {

src/applications/model/udp-server.cc
@@ -56,6 +56,7 @@ UdpServer::UdpServer()
     : SinkApplication(DEFAULT_PORT)
 {
     NS_LOG_FUNCTION(this);
+    m_tid = TypeId::LookupByName("ns3::UdpSocketFactory");
 }
 
 UdpServer::~UdpServer()
@@ -95,8 +96,7 @@ UdpServer::StartApplication()
 
     if (!m_socket)
     {
-        auto tid = TypeId::LookupByName("ns3::UdpSocketFactory");
-        m_socket = Socket::CreateSocket(GetNode(), tid);
+        m_socket = Socket::CreateSocket(GetNode(), m_tid);
         auto local = m_local;
         if (local.IsInvalid())
         {
@@ -129,8 +129,7 @@ UdpServer::StartApplication()
     {
         // local address is not specified, so create another socket to also listen to all IPv6
         // addresses
-        auto tid = TypeId::LookupByName("ns3::UdpSocketFactory");
-        m_socket6 = Socket::CreateSocket(GetNode(), tid);
+        m_socket6 = Socket::CreateSocket(GetNode(), m_tid);
         auto local = Inet6SocketAddress(Ipv6Address::GetAny(), m_port);
         if (m_socket6->Bind(local) == -1)
         {

sjeon87/ns-3-dev

src/applications/model/three-gpp-http-server.cc
@@ -40,6 +40,7 @@ ThreeGppHttpServer::ThreeGppHttpServer()
 {
     NS_LOG_FUNCTION(this);
     NS_LOG_INFO(this << " MTU size for this server application is " << m_mtuSize << " bytes.");
+    m_protocolTid = TypeId::LookupByName("ns3::TcpSocketFactory");
 }
 
 // static
@@ -225,7 +226,7 @@ ThreeGppHttpServer::StartApplication()
     if (!m_socket)
     {
         // Creating a TCP socket to connect to the server.
-        m_socket = Socket::CreateSocket(GetNode(), TcpSocketFactory::GetTypeId());
+        m_socket = Socket::CreateSocket(GetNode(), m_protocolTid);
         m_socket->SetAttribute("SegmentSize", UintegerValue(m_mtuSize));
 
         NS_ABORT_MSG_IF(m_local.IsInvalid(), "Local address not properly set");

src/applications/model/udp-echo-server.cc
@@ -52,6 +52,7 @@ UdpEchoServer::UdpEchoServer()
     : SinkApplication(DEFAULT_PORT)
 {
     NS_LOG_FUNCTION(this);
+    m_protocolTid = TypeId::LookupByName("ns3::UdpSocketFactory");
 }
 
 UdpEchoServer::~UdpEchoServer()
@@ -66,8 +67,7 @@ UdpEchoServer::StartApplication()
 
     if (!m_socket)
     {
-        auto tid = TypeId::LookupByName("ns3::UdpSocketFactory");
-        m_socket = Socket::CreateSocket(GetNode(), tid);
+        m_socket = Socket::CreateSocket(GetNode(), m_protocolTid);
         auto local = m_local;
         if (local.IsInvalid())
         {
@@ -98,8 +98,7 @@ UdpEchoServer::StartApplication()
     {
         // local address is not specified, so create another socket to also listen to all IPv6
         // addresses
-        auto tid = TypeId::LookupByName("ns3::UdpSocketFactory");
-        m_socket6 = Socket::CreateSocket(GetNode(), tid);
+        m_socket6 = Socket::CreateSocket(GetNode(), m_protocolTid);
         auto local = Inet6SocketAddress(Ipv6Address::GetAny(), m_port);
         if (m_socket6->Bind(local) == -1)
         {

src/applications/model/udp-server.cc
@@ -56,6 +56,7 @@ UdpServer::UdpServer()
     : SinkApplication(DEFAULT_PORT)
 {
     NS_LOG_FUNCTION(this);
+    m_protocolTid = TypeId::LookupByName("ns3::UdpSocketFactory");
 }
 
 UdpServer::~UdpServer()
@@ -95,8 +96,7 @@ UdpServer::StartApplication()
 
     if (!m_socket)
     {
-        auto tid = TypeId::LookupByName("ns3::UdpSocketFactory");
-        m_socket = Socket::CreateSocket(GetNode(), tid);
+        m_socket = Socket::CreateSocket(GetNode(), m_protocolTid);
         auto local = m_local;
         if (local.IsInvalid())
         {
@@ -129,8 +129,7 @@ UdpServer::StartApplication()
     {
         // local address is not specified, so create another socket to also listen to all IPv6
         // addresses
-        auto tid = TypeId::LookupByName("ns3::UdpSocketFactory");
-        m_socket6 = Socket::CreateSocket(GetNode(), tid);
+        m_socket6 = Socket::CreateSocket(GetNode(), m_protocolTid);
         auto local = Inet6SocketAddress(Ipv6Address::GetAny(), m_port);
         if (m_socket6->Bind(local) == -1)
         {