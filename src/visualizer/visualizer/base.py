0d52d50fc59f8fde4fdfa3327478f3ac6aaf6c56
Usham Roy <ushamroy80@gmail.com>
2026-03-15 22:32:36 +0530

    visualizer: Fix MAC address handling for non-MAC-48 devices
    Add VirtualNetDevice to netdevice_traits and use
    Mac48Address.IsMatchingType() instead of try/except for
    devices without MAC-48 addresses.
    
    Refs: #1017

--- src/visualizer/visualizer/base.py
+++ src/visualizer/visualizer/base.py
@@ -61,6 +61,7 @@ netdevice_traits = {
     ns.MeshPointDevice: NetDeviceTraits(is_virtual=True),
     ns.LteUeNetDevice: NetDeviceTraits(is_wireless=True),
     ns.LteEnbNetDevice: NetDeviceTraits(is_wireless=True),
+    ns.VirtualNetDevice: NetDeviceTraits(is_virtual=True),
     ns.lrwpan.LrWpanNetDevice: NetDeviceTraits(is_wireless=True),
     ns.SixLowPanNetDevice: NetDeviceTraits(is_virtual=False, is_wireless=True),
 }
