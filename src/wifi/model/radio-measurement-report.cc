fcc1811fb44517af27f42ce8791845d8ce20bdec
Devansh Gupta <devanshg308@gmail.com>
2026-03-20 02:59:30 +0530

    wifi: Add RadioMeasurementReportHeader (Action 1, Section 9.6.6.3)

---
+++ src/wifi/model/radio-measurement-report.cc
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2025
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *
+ * Author: Devansh Gupta <devanshg308@gmail.com>
+ */
+
+#include "radio-measurement-report.h"
+
+#include "ns3/log.h"
+
+namespace ns3
+{
+
+NS_LOG_COMPONENT_DEFINE("RadioMeasurementReportHeader");
+NS_OBJECT_ENSURE_REGISTERED(RadioMeasurementReportHeader);
+
+TypeId
+RadioMeasurementReportHeader::GetTypeId()
+{
+    static TypeId tid = TypeId("ns3::RadioMeasurementReportHeader")
+                            .SetParent<Header>()
+                            .SetGroupName("Wifi")
+                            .AddConstructor<RadioMeasurementReportHeader>();
+    return tid;
+}
+
+TypeId
+RadioMeasurementReportHeader::GetInstanceTypeId() const
+{
+    return GetTypeId();
+}
+
+void
+RadioMeasurementReportHeader::Print(std::ostream& os) const
+{
+    os << "DialogToken=" << +m_dialogToken
+       << ", MeasurementReportElements=" << m_measurementReportElements.size();
+}
+
+uint32_t
+RadioMeasurementReportHeader::GetSerializedSize() const
+{
+    uint32_t size = 1; // Dialog Token (1)
+    for (const auto& elem : m_measurementReportElements)
+    {
+        size += elem.GetSerializedSize();
+    }
+    return size;
+}
+
+void
+RadioMeasurementReportHeader::Serialize(Buffer::Iterator start) const
+{
+    Buffer::Iterator i = start;
+    i.WriteU8(m_dialogToken);
+    for (const auto& elem : m_measurementReportElements)
+    {
+        i = elem.Serialize(i);
+    }
+}
+
+uint32_t
+RadioMeasurementReportHeader::Deserialize(Buffer::Iterator start)
+{
+    Buffer::Iterator i = start;
+    m_dialogToken = i.ReadU8();
+
+    m_measurementReportElements.clear();
+    while (true)
+    {
+        auto tmp = i;
+        MeasurementReportElement elem;
+        i = elem.DeserializeIfPresent(i);
+        if (i.GetDistanceFrom(tmp) == 0)
+        {
+            break;
+        }
+        m_measurementReportElements.push_back(std::move(elem));
+    }
+
+    return i.GetDistanceFrom(start);
+}
+
+void
+RadioMeasurementReportHeader::SetDialogToken(uint8_t token)
+{
+    m_dialogToken = token;
+}
+
+uint8_t
+RadioMeasurementReportHeader::GetDialogToken() const
+{
+    return m_dialogToken;
+}
+
+void
+RadioMeasurementReportHeader::AddMeasurementReportElement(const MeasurementReportElement& elem)
+{
+    m_measurementReportElements.push_back(elem);
+}
+
+const std::vector<MeasurementReportElement>&
+RadioMeasurementReportHeader::GetMeasurementReportElements() const
+{
+    return m_measurementReportElements;
+}
+
+} // namespace ns3
---
+++ src/wifi/model/radio-measurement-report.h
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2025
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *
+ * Author: Devansh Gupta <devanshg308@gmail.com>
+ */
+
+#ifndef RADIO_MEASUREMENT_REPORT_H
+#define RADIO_MEASUREMENT_REPORT_H
+
+#include "measurement-report-element.h"
+
+#include "ns3/header.h"
+
+#include <vector>
+
+namespace ns3
+{
+
+/**
+ * @brief Radio Measurement Report frame body
+ * @ingroup wifi
+ *
+ * IEEE 802.11-2024 Section 9.6.6.3, Figure 9-1186.
+ *
+ * Fields after Category + Action (handled by WifiActionHeader):
+ * - Dialog Token (1 octet)
+ * - Measurement Report Elements (variable, one or more IE 39)
+ */
+class RadioMeasurementReportHeader : public Header
+{
+  public:
+    /**
+     * Register this type.
+     * @return The TypeId.
+     */
+    static TypeId GetTypeId();
+    TypeId GetInstanceTypeId() const override;
+    void Print(std::ostream& os) const override;
+    uint32_t GetSerializedSize() const override;
+    void Serialize(Buffer::Iterator start) const override;
+    uint32_t Deserialize(Buffer::Iterator start) override;
+
+    /**
+     * @param token the dialog token (matches request token, or 0 for autonomous reports)
+     */
+    void SetDialogToken(uint8_t token);
+    /**
+     * @return the dialog token
+     */
+    uint8_t GetDialogToken() const;
+
+    /**
+     * @param elem a Measurement Report element to append
+     */
+    void AddMeasurementReportElement(const MeasurementReportElement& elem);
+    /**
+     * @return the vector of Measurement Report elements
+     */
+    const std::vector<MeasurementReportElement>& GetMeasurementReportElements() const;
+
+  private:
+    uint8_t m_dialogToken{0}; //!< Dialog Token (1 octet)
+    std::vector<MeasurementReportElement>
+        m_measurementReportElements; //!< Measurement Report elements
+};
+
+} // namespace ns3
+
+#endif // RADIO_MEASUREMENT_REPORT_H
