nsnam/ns-3-dev-git

wifi: A STA resumes from sleep when it has to request channel access

src/wifi/model/default-power-save-manager.cc
@@ -9,6 +9,7 @@
 #include "default-power-save-manager.h"
 
 #include "sta-wifi-mac.h"
+#include "wifi-mpdu.h"
 #include "wifi-phy.h"
 
 #include "ns3/log.h"
@@ -157,6 +158,24 @@ DefaultPowerSaveManager::DoNotifyReceivedGroupcast(Ptr<const WifiMpdu> mpdu, uin
     NS_LOG_FUNCTION(this << mpdu << linkId);
 
     GoToSleepIfPossible(linkId);
+}
+
+void
+DefaultPowerSaveManager::DoNotifyRequestAccess(Ptr<Txop> txop, uint8_t linkId)
+{
+    NS_LOG_FUNCTION(this << txop << linkId);
+
+    // if channel access is being requested, it means that the MAC has frames to transmit on the
+    // given link, hence wake up the PHY if it is in sleep state
+    if (auto phy = GetStaMac()->GetWifiPhy(linkId); phy->IsStateSleep())
+    {
+        NS_LOG_DEBUG("Resume from sleep STA operating on link " << +linkId);
+        phy->ResumeFromSleep();
+        if (auto it = m_wakeUpEvents.find(linkId); it != m_wakeUpEvents.cend())
+        {
+            it->second.Cancel();
+        }
+    }
 }
 
 } // namespace ns3

src/wifi/model/default-power-save-manager.h
@@ -44,6 +44,7 @@ class DefaultPowerSaveManager : public PowerSaveManager
     void DoNotifyReceivedBeacon(const MgtBeaconHeader& beacon, uint8_t linkId) override;
     void DoNotifyReceivedFrameAfterPsPoll(Ptr<const WifiMpdu> mpdu, uint8_t linkId) override;
     void DoNotifyReceivedGroupcast(Ptr<const WifiMpdu> mpdu, uint8_t linkId) override;
+    void DoNotifyRequestAccess(Ptr<Txop> txop, uint8_t linkId) override;
 
     std::map<uint8_t, EventId> m_wakeUpEvents; ///< events scheduled to wake up PHYs
 };

src/wifi/model/power-save-manager.h
@@ -193,6 +193,14 @@ class PowerSaveManager : public Object
      */
     virtual void DoNotifyReceivedGroupcast(Ptr<const WifiMpdu> mpdu, uint8_t linkId) = 0;
 
+    /**
+     * Notify subclasses that the given TXOP is requesting channel access on the given link.
+     *
+     * @param txop the DCF/EDCAF requesting channel access
+     * @param linkId the ID of the given link
+     */
+    virtual void DoNotifyRequestAccess(Ptr<Txop> txop, uint8_t linkId) = 0;
+
   private:
     Ptr<StaWifiMac> m_staMac;             //!< MAC which is using this Power Save Manager
     std::map<uint8_t, StaInfo> m_staInfo; ///< link ID-indexed map of STA infos

sjeon87/ns-3-dev

src/wifi/model/default-power-save-manager.cc
@@ -9,6 +9,7 @@
 #include "default-power-save-manager.h"
 
 #include "sta-wifi-mac.h"
+#include "wifi-mpdu.h"
 #include "wifi-phy.h"
 
 #include "ns3/log.h"
@@ -158,6 +159,24 @@ DefaultPowerSaveManager::DoNotifyReceivedGroupcast(Ptr<const WifiMpdu> mpdu, lin
     NS_LOG_FUNCTION(this << mpdu << linkId);
 
     GoToSleepIfPossible(linkId);
+}
+
+void
+DefaultPowerSaveManager::DoNotifyRequestAccess(Ptr<Txop> txop, linkId_t linkId)
+{
+    NS_LOG_FUNCTION(this << txop << linkId);
+
+    // if channel access is being requested, it means that the MAC has frames to transmit on the
+    // given link, hence wake up the PHY if it is in sleep state
+    if (auto phy = GetStaMac()->GetWifiPhy(linkId); phy->IsStateSleep())
+    {
+        NS_LOG_DEBUG("Resume from sleep STA operating on link " << +linkId);
+        phy->ResumeFromSleep();
+        if (auto it = m_wakeUpEvents.find(linkId); it != m_wakeUpEvents.cend())
+        {
+            it->second.Cancel();
+        }
+    }
 }
 
 } // namespace ns3

src/wifi/model/default-power-save-manager.h
@@ -44,6 +44,7 @@ class DefaultPowerSaveManager : public PowerSaveManager
     void DoNotifyReceivedBeacon(const MgtBeaconHeader& beacon, linkId_t linkId) override;
     void DoNotifyReceivedFrameAfterPsPoll(Ptr<const WifiMpdu> mpdu, linkId_t linkId) override;
     void DoNotifyReceivedGroupcast(Ptr<const WifiMpdu> mpdu, linkId_t linkId) override;
+    void DoNotifyRequestAccess(Ptr<Txop> txop, linkId_t linkId) override;
 
     std::map<linkId_t, EventId> m_wakeUpEvents; ///< events scheduled to wake up PHYs
 };

src/wifi/model/power-save-manager.h
@@ -196,6 +196,14 @@ class PowerSaveManager : public Object
      */
     virtual void DoNotifyReceivedGroupcast(Ptr<const WifiMpdu> mpdu, linkId_t linkId) = 0;
 
+    /**
+     * Notify subclasses that the given TXOP is requesting channel access on the given link.
+     *
+     * @param txop the DCF/EDCAF requesting channel access
+     * @param linkId the ID of the given link
+     */
+    virtual void DoNotifyRequestAccess(Ptr<Txop> txop, linkId_t linkId) = 0;
+
   private:
     Ptr<StaWifiMac> m_staMac;              //!< MAC which is using this Power Save Manager
     std::map<linkId_t, StaInfo> m_staInfo; ///< link ID-indexed map of STA infos