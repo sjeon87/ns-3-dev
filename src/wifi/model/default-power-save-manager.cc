nsnam/ns-3-dev-git

wifi: A STA in PS mode goes to sleep (if needed) when the AP is done sending broadcast frames

src/wifi/model/default-power-save-manager.cc
@@ -151,4 +151,12 @@ DefaultPowerSaveManager::DoNotifyReceivedFrameAfterPsPoll(Ptr<const WifiMpdu> mp
     }
 }
 
+void
+DefaultPowerSaveManager::DoNotifyReceivedGroupcast(Ptr<const WifiMpdu> mpdu, uint8_t linkId)
+{
+    NS_LOG_FUNCTION(this << mpdu << linkId);
+
+    GoToSleepIfPossible(linkId);
+}
+
 } // namespace ns3

src/wifi/model/default-power-save-manager.h
@@ -43,6 +43,7 @@ class DefaultPowerSaveManager : public PowerSaveManager
     void DoNotifyDisassociation() override;
     void DoNotifyReceivedBeacon(const MgtBeaconHeader& beacon, uint8_t linkId) override;
     void DoNotifyReceivedFrameAfterPsPoll(Ptr<const WifiMpdu> mpdu, uint8_t linkId) override;
+    void DoNotifyReceivedGroupcast(Ptr<const WifiMpdu> mpdu, uint8_t linkId) override;
 
     std::map<uint8_t, EventId> m_wakeUpEvents; ///< events scheduled to wake up PHYs
 };

src/wifi/model/power-save-manager.h
@@ -184,6 +184,15 @@ class PowerSaveManager : public Object
      */
     virtual void DoNotifyReceivedFrameAfterPsPoll(Ptr<const WifiMpdu> mpdu, uint8_t linkId) = 0;
 
+    /**
+     * Notify subclasses of the reception of a groupcast frame (possibly after a DTIM) on the given
+     * link.
+     *
+     * @param mpdu the received MPDU
+     * @param linkId the ID of the given link
+     */
+    virtual void DoNotifyReceivedGroupcast(Ptr<const WifiMpdu> mpdu, uint8_t linkId) = 0;
+
   private:
     Ptr<StaWifiMac> m_staMac;             //!< MAC which is using this Power Save Manager
     std::map<uint8_t, StaInfo> m_staInfo; ///< link ID-indexed map of STA infos

sjeon87/ns-3-dev

src/wifi/model/default-power-save-manager.cc
@@ -152,4 +152,12 @@ DefaultPowerSaveManager::DoNotifyReceivedFrameAfterPsPoll(Ptr<const WifiMpdu> mp
     }
 }
 
+void
+DefaultPowerSaveManager::DoNotifyReceivedGroupcast(Ptr<const WifiMpdu> mpdu, linkId_t linkId)
+{
+    NS_LOG_FUNCTION(this << mpdu << linkId);
+
+    GoToSleepIfPossible(linkId);
+}
+
 } // namespace ns3

src/wifi/model/default-power-save-manager.h
@@ -43,6 +43,7 @@ class DefaultPowerSaveManager : public PowerSaveManager
     void DoNotifyDisassociation() override;
     void DoNotifyReceivedBeacon(const MgtBeaconHeader& beacon, linkId_t linkId) override;
     void DoNotifyReceivedFrameAfterPsPoll(Ptr<const WifiMpdu> mpdu, linkId_t linkId) override;
+    void DoNotifyReceivedGroupcast(Ptr<const WifiMpdu> mpdu, linkId_t linkId) override;
 
     std::map<linkId_t, EventId> m_wakeUpEvents; ///< events scheduled to wake up PHYs
 };

src/wifi/model/power-save-manager.h
@@ -187,6 +187,15 @@ class PowerSaveManager : public Object
      */
     virtual void DoNotifyReceivedFrameAfterPsPoll(Ptr<const WifiMpdu> mpdu, linkId_t linkId) = 0;
 
+    /**
+     * Notify subclasses of the reception of a groupcast frame (possibly after a DTIM) on the given
+     * link.
+     *
+     * @param mpdu the received MPDU
+     * @param linkId the ID of the given link
+     */
+    virtual void DoNotifyReceivedGroupcast(Ptr<const WifiMpdu> mpdu, linkId_t linkId) = 0;
+
   private:
     Ptr<StaWifiMac> m_staMac;              //!< MAC which is using this Power Save Manager
     std::map<linkId_t, StaInfo> m_staInfo; ///< link ID-indexed map of STA infos