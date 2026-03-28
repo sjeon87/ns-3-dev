nsnam/ns-3-dev-git

wifi: Put PHY to sleep or enqueue PS Poll when receiving frame in response to PS Poll

src/wifi/model/default-power-save-manager.cc
@@ -135,4 +135,20 @@ DefaultPowerSaveManager::DoNotifyReceivedBeacon(const MgtBeaconHeader& beacon, u
     }
 }
 
+void
+DefaultPowerSaveManager::DoNotifyReceivedFrameAfterPsPoll(Ptr<const WifiMpdu> mpdu, uint8_t linkId)
+{
+    NS_LOG_FUNCTION(this << mpdu << linkId);
+
+    if (GetStaInfo(linkId).pendingUnicast)
+    {
+        NS_LOG_LOGIC("Waiting for more unicast frames; enqueue a PS-Poll frame");
+        GetStaMac()->EnqueuePsPoll(linkId);
+    }
+    else
+    {
+        GoToSleepIfPossible(linkId);
+    }
+}
+
 } // namespace ns3

src/wifi/model/default-power-save-manager.h
@@ -42,6 +42,7 @@ class DefaultPowerSaveManager : public PowerSaveManager
     void DoNotifyAssocCompleted() override;
     void DoNotifyDisassociation() override;
     void DoNotifyReceivedBeacon(const MgtBeaconHeader& beacon, uint8_t linkId) override;
+    void DoNotifyReceivedFrameAfterPsPoll(Ptr<const WifiMpdu> mpdu, uint8_t linkId) override;
 
     std::map<uint8_t, EventId> m_wakeUpEvents; ///< events scheduled to wake up PHYs
 };

src/wifi/model/power-save-manager.h
@@ -175,6 +175,15 @@ class PowerSaveManager : public Object
      */
     virtual void DoNotifyReceivedBeacon(const MgtBeaconHeader& beacon, uint8_t linkId) = 0;
 
+    /**
+     * Notify subclasses of the reception of a frame in response to a PS-Poll frame on the given
+     * link. The notification is sent a SIFS after the reception of the frame.
+     *
+     * @param mpdu the received MPDU
+     * @param linkId the ID of the given link
+     */
+    virtual void DoNotifyReceivedFrameAfterPsPoll(Ptr<const WifiMpdu> mpdu, uint8_t linkId) = 0;
+
   private:
     Ptr<StaWifiMac> m_staMac;             //!< MAC which is using this Power Save Manager
     std::map<uint8_t, StaInfo> m_staInfo; ///< link ID-indexed map of STA infos

sjeon87/ns-3-dev

src/wifi/model/default-power-save-manager.cc
@@ -136,4 +136,20 @@ DefaultPowerSaveManager::DoNotifyReceivedBeacon(const MgtBeaconHeader& beacon, l
     }
 }
 
+void
+DefaultPowerSaveManager::DoNotifyReceivedFrameAfterPsPoll(Ptr<const WifiMpdu> mpdu, linkId_t linkId)
+{
+    NS_LOG_FUNCTION(this << mpdu << linkId);
+
+    if (GetStaInfo(linkId).pendingUnicast)
+    {
+        NS_LOG_LOGIC("Waiting for more unicast frames; enqueue a PS-Poll frame");
+        GetStaMac()->EnqueuePsPoll(linkId);
+    }
+    else
+    {
+        GoToSleepIfPossible(linkId);
+    }
+}
+
 } // namespace ns3

src/wifi/model/default-power-save-manager.h
@@ -42,6 +42,7 @@ class DefaultPowerSaveManager : public PowerSaveManager
     void DoNotifyAssocCompleted() override;
     void DoNotifyDisassociation() override;
     void DoNotifyReceivedBeacon(const MgtBeaconHeader& beacon, linkId_t linkId) override;
+    void DoNotifyReceivedFrameAfterPsPoll(Ptr<const WifiMpdu> mpdu, linkId_t linkId) override;
 
     std::map<linkId_t, EventId> m_wakeUpEvents; ///< events scheduled to wake up PHYs
 };

src/wifi/model/power-save-manager.h
@@ -178,6 +178,15 @@ class PowerSaveManager : public Object
      */
     virtual void DoNotifyReceivedBeacon(const MgtBeaconHeader& beacon, linkId_t linkId) = 0;
 
+    /**
+     * Notify subclasses of the reception of a frame in response to a PS-Poll frame on the given
+     * link. The notification is sent a SIFS after the reception of the frame.
+     *
+     * @param mpdu the received MPDU
+     * @param linkId the ID of the given link
+     */
+    virtual void DoNotifyReceivedFrameAfterPsPoll(Ptr<const WifiMpdu> mpdu, linkId_t linkId) = 0;
+
   private:
     Ptr<StaWifiMac> m_staMac;              //!< MAC which is using this Power Save Manager
     std::map<linkId_t, StaInfo> m_staInfo; ///< link ID-indexed map of STA infos