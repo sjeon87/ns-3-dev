dad7450f8b558c4f2ecf4b364d2259d243c07096
Kedar Kisan <kedarkisan2020@gmail.com>
2026-04-01 01:31:33 +0000

    fix: Use (void) cast to safely suppress unused variable warnings without breaking clang-format

--- src/applications/model/flent-application.cc
+++ src/applications/model/flent-application.cc
@@ -240,7 +240,7 @@ FlentApplication::AddMetadata(Json::Value& j)
 }
 
 Ptr<Node>
-FlentApplication::GetHostNode([[maybe_unused]] Ipv4Address hostAddress) const
+FlentApplication::GetHostNode(Ipv4Address hostAddress) const
 {
     NS_LOG_FUNCTION(this << hostAddress);
 
@@ -474,7 +474,8 @@ FlentApplication::StartApplication(void) // Called at time specified by Start
 
     if (m_testName.compare("ping") == 0)
     {
-        [[maybe_unused]] Ipv4Address hostAddr = Ipv4Address::ConvertFrom(m_hostAddress);
+        Ipv4Address hostAddr = Ipv4Address::ConvertFrom(m_hostAddress);
+        (void) hostAddr;
         m_v4ping = CreateObject<Ping>();
         m_v4ping->SetAttribute("Destination", AddressValue(m_hostAddress));
         m_v4ping->SetAttribute("Interval", TimeValue(m_stepSize));
@@ -489,7 +490,8 @@ FlentApplication::StartApplication(void) // Called at time specified by Start
     }
     else if (m_testName.compare("tcp_upload") == 0)
     {
-        [[maybe_unused]] Ipv4Address hostAddr = Ipv4Address::ConvertFrom(m_hostAddress);
+        Ipv4Address hostAddr = Ipv4Address::ConvertFrom(m_hostAddress);
+        (void) hostAddr;
         m_v4ping = CreateObject<Ping>();
         m_v4ping->SetAttribute("Destination", AddressValue(m_hostAddress));
         m_v4ping->SetAttribute("Interval", TimeValue(m_stepSize));
@@ -545,7 +547,8 @@ FlentApplication::StartApplication(void) // Called at time specified by Start
     }
     else if (m_testName.compare("tcp_download") == 0)
     {
-        [[maybe_unused]] Ipv4Address localBindAddr = Ipv4Address::ConvertFrom(m_localBindAddress);
+        Ipv4Address localBindAddr = Ipv4Address::ConvertFrom(m_localBindAddress);
+        (void) localBindAddr;
         m_v4ping = CreateObject<Ping>();
         m_v4ping->SetAttribute("Destination", AddressValue(m_localBindAddress));
         m_v4ping->SetAttribute("Interval", TimeValue(m_stepSize));
@@ -600,8 +603,10 @@ FlentApplication::StartApplication(void) // Called at time specified by Start
     }
     else if (m_testName.compare("rrul") == 0)
     {
-        [[maybe_unused]] Ipv4Address hostIpv4Address = Ipv4Address::ConvertFrom(m_hostAddress);
-        [[maybe_unused]] Ipv4Address localIpv4Address = Ipv4Address::ConvertFrom(m_localBindAddress);
+        Ipv4Address hostIpv4Address = Ipv4Address::ConvertFrom(m_hostAddress);
+        (void) hostIpv4Address;
+        Ipv4Address localIpv4Address = Ipv4Address::ConvertFrom(m_localBindAddress);
+        (void) localIpv4Address;
 
         m_v4ping = CreateObject<Ping>();
         m_v4ping->SetAttribute("Destination", AddressValue(m_hostAddress));
