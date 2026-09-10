nsnam/ns-3-dev-git

wifi: AdhocWifiMac records capabilities from probe requests

src/wifi/model/adhoc-wifi-mac.h
@@ -91,6 +91,14 @@ class AdhocWifiMac : public WifiMac
     void ReceiveBeacon(Ptr<const WifiMpdu> mpdu, uint8_t linkId);
 
     /**
+     * Process the Probe Request frame received on the given link.
+     *
+     * \param mpdu the MPDU containing the Probe Request frame
+     * \param linkId the ID of the given link
+     */
+    void ReceiveProbeRequest(Ptr<const WifiMpdu> mpdu, uint8_t linkId);
+
+    /**
      * Fill in the same capabilities as this device for a given new peer.
      *
      * \param address the MAC address of the peer

sjeon87/ns-3-dev

src/wifi/model/adhoc-wifi-mac.h
@@ -106,6 +106,14 @@ class AdhocWifiMac : public WifiMac
     void ReceiveBeacon(Ptr<const WifiMpdu> mpdu, linkId_t linkId);
 
     /**
+     * Process the Probe Request frame received on the given link.
+     *
+     * @param mpdu the MPDU containing the Probe Request frame
+     * @param linkId the ID of the given link
+     */
+    void ReceiveProbeRequest(Ptr<const WifiMpdu> mpdu, linkId_t linkId);
+
+    /**
      * Fill in the same capabilities as this device for a given new peer.
      *
      * @param address the MAC address of the peer