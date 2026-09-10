nsnam/ns-3-dev-git

wifi: Allow to call RecordCapabilities for more management frame types

src/wifi/model/sta-wifi-mac.cc
@@ -1664,14 +1664,17 @@ StaWifiMac::CheckSupportedRates(std::variant<MgtBeaconHeader, MgtProbeResponseHe
 }
 
 void
-StaWifiMac::UpdateApInfo(const MgtFrameType& frame,
+StaWifiMac::UpdateApInfo(const MgtResponseFrameType& frame,
                          const Mac48Address& apAddr,
                          const Mac48Address& bssid,
                          uint8_t linkId)
 {
     NS_LOG_FUNCTION(this << frame.index() << apAddr << bssid << +linkId);
 
-    RecordCapabilities(frame, apAddr, linkId);
+    auto doRecordCapabilities = [&](auto&& mgtFrame) {
+        RecordCapabilities(mgtFrame, apAddr, linkId);
+    };
+    std::visit(doRecordCapabilities, frame);
 
     // ERP Information is not present in Association Response frames
     const std::optional<ErpInformation>* erpInformation = nullptr;

src/wifi/model/sta-wifi-mac.h
@@ -153,10 +153,10 @@ class StaWifiMac : public WifiMac
             Mac48Address bssid;  ///< BSSID
         };
 
-        Mac48Address m_bssid;  ///< BSSID
-        Mac48Address m_apAddr; ///< AP MAC address
-        double m_snr;          ///< SNR in linear scale
-        MgtFrameType m_frame;  ///< The body of the management frame used to update AP info
+        Mac48Address m_bssid;         ///< BSSID
+        Mac48Address m_apAddr;        ///< AP MAC address
+        double m_snr;                 ///< SNR in linear scale
+        MgtResponseFrameType m_frame; ///< The body of the management frame used to update AP info
         WifiScanParams::Channel m_channel; ///< The channel the management frame was received on
         uint8_t m_linkId;                  ///< ID of the link used to communicate with the AP
         std::list<SetupLinksInfo>
@@ -457,7 +457,7 @@ class StaWifiMac : public WifiMac
      * @param bssid MAC address of BSSID
      * @param linkId ID of the link the management frame was received over
      */
-    void UpdateApInfo(const MgtFrameType& frame,
+    void UpdateApInfo(const MgtResponseFrameType& frame,
                       const Mac48Address& apAddr,
                       const Mac48Address& bssid,
                       uint8_t linkId);

sjeon87/ns-3-dev

src/wifi/model/sta-wifi-mac.cc
@@ -1884,7 +1884,9 @@ StaWifiMac::CheckSupportedRates(std::variant<MgtBeaconHeader, MgtProbeResponseHe
 }
 
 void
-StaWifiMac::RecordOperations(const MgtFrameType& frame, const Mac48Address& from, uint8_t linkId)
+StaWifiMac::RecordOperations(const MgtResponseFrameType& frame,
+                             const Mac48Address& from,
+                             linkId_t linkId)
 {
     NS_LOG_FUNCTION(this << frame.index() << from << linkId);
     auto remoteStationManager = GetWifiRemoteStationManager(linkId);
@@ -1940,7 +1942,7 @@ StaWifiMac::RecordOperations(const MgtFrameType& frame, const Mac48Address& from
 }
 
 void
-StaWifiMac::ApplyOperationalSettings(const MgtFrameType& frame,
+StaWifiMac::ApplyOperationalSettings(const MgtResponseFrameType& frame,
                                      const Mac48Address& apAddr,
                                      const Mac48Address& bssid,
                                      uint8_t linkId)

src/wifi/model/sta-wifi-mac.h
@@ -167,10 +167,10 @@ class StaWifiMac : public WifiMac
             Mac48Address bssid;  ///< BSSID
         };
 
-        Mac48Address m_bssid;  ///< BSSID
-        Mac48Address m_apAddr; ///< AP MAC address
-        double m_snr;          ///< SNR in linear scale
-        MgtFrameType m_frame;  ///< The body of the management frame used to update AP info
+        Mac48Address m_bssid;         ///< BSSID
+        Mac48Address m_apAddr;        ///< AP MAC address
+        double m_snr;                 ///< SNR in linear scale
+        MgtResponseFrameType m_frame; ///< The body of the management frame used to update AP info
         WifiScanParams::Channel m_channel; ///< The channel the management frame was received on
         uint8_t m_linkId;                  ///< ID of the link used to communicate with the AP
         std::list<SetupLinksInfo>
@@ -517,7 +517,9 @@ class StaWifiMac : public WifiMac
      * @param addr MAC address of the sender
      * @param linkId ID of the link the management frame was received over
      */
-    void RecordOperations(const MgtFrameType& frame, const Mac48Address& addr, uint8_t linkId);
+    void RecordOperations(const MgtResponseFrameType& frame,
+                          const Mac48Address& addr,
+                          linkId_t linkId);
 
     /**
      * Update operational settings based on associated AP's information provided by the given
@@ -528,7 +530,7 @@ class StaWifiMac : public WifiMac
      * @param bssid MAC address of BSSID
      * @param linkId ID of the link the management frame was received over
      */
-    void ApplyOperationalSettings(const MgtFrameType& frame,
+    void ApplyOperationalSettings(const MgtResponseFrameType& frame,
                                   const Mac48Address& apAddr,
                                   const Mac48Address& bssid,
                                   uint8_t linkId);