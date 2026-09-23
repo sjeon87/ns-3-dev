13483119864eac1a99a5d91d37897627d9188986
Seth Jeon <git@sethjeon.com>
2026-09-23 01:39:00 +0000

    Update quick rebuild script to log progress and results

--- rebuild.sh
+++ rebuild.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-./ns3 clean && ./ns3 configure --enable-tests --enable-examples > configure.log 2>&1 && ./ns3 build > build.log 2>&1
+if ! command -v ansifilter >/dev/null 2>&1; then
+  echo "Warning: ansifilter is not found. ANSI escape codes will be written to logs." >&2
+  ansifilter() { cat; }
+fi
+
+./ns3 clean && NO_COLOR=1 ./ns3 configure --enable-tests --enable-examples 2>&1 | ansifilter > configure.log && NO_COLOR=1 ./ns3 build 2>&1 | ansifilter > build.log
